36dd119a82b776c311f821cbee94760f103ca8d9
icarodredd <icarog854@gmail.com>
2024-08-08 09:27:25 -0300

    catching questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import Main from "./components/Main";
+
 function App() {
   return (
-    <h1 className="text-center text-3xl font-sans font-bold text-white">
-      React Quiz
-    </h1>
+    <>
+      <h1 className="text-center text-3xl font-sans font-bold text-white">
+        React Quiz
+      </h1>
+      <Main />
+    </>
   );
 }
 
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,1 +1,26 @@
-export default function Main() {}
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  loading: false,
+};
+
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "received":
+      return { ...state, loading: false, questions: action.payload };
+  }
+};
+
+export default function Main() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((json) => dispatch({ type: "received", payload: json }))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return <h1>oi</h1>;
+}
